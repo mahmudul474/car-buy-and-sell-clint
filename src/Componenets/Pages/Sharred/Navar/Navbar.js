9d812b2fe32581248ba833d6c281d5b69508bdef
devSobuj910 <ftsobuj222@gmail.com>
2022-11-24 17:50:28 +0600

    hom  baner create

--- src/Componenets/Pages/Sharred/Navar/Navbar.js
+++ src/Componenets/Pages/Sharred/Navar/Navbar.js
@@ -27,19 +27,15 @@ const Navbar = () => {
         {navlink}
       </ul>
     </div>
-    <a className=" ">
+    <a className=" /#">
       <img src={logo }  className=" h-20 object-contain lg:h-34" alt="main-logo" />
     </a>
   </div>
-  <div className="navbar-center hidden lg:flex">
+  <div className="navbar-end hidden lg:flex">
     <ul className="menu menu-horizontal p-0">
       
       {navlink}
-      
-    </ul>
-  </div>
-  <div className="navbar-end">
-  <div className="dropdown dropdown-end">
+      <div className="dropdown dropdown-end">
       <label tabIndex={0} className="btn btn-ghost btn-circle">
         <div className="indicator">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
@@ -73,7 +69,10 @@ const Navbar = () => {
         <li><a>Logout</a></li>
       </ul>
     </div>
+      
+    </ul>
   </div>
+ 
 </div>
 
 
