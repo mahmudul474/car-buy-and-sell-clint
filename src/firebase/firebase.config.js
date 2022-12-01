7ea9493a060f3a976c97b657d55d558cca3b455d
devSobuj910 <ftsobuj222@gmail.com>
2022-12-02 04:35:43 +0600

    data load chck

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,15 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_apiKey,
-  authDomain:process.env.REACT_APP_authDomain,
-  projectId:process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId,
+  apiKey: "AIzaSyAUynpBcjo-hvz4BGscs2EXog80RWoaRVA",
+  authDomain: "car-sell-buy-client.firebaseapp.com",
+  projectId: "car-sell-buy-client",
+  storageBucket: "car-sell-buy-client.appspot.com",
+  messagingSenderId: "257291104023",
+  appId: "1:257291104023:web:a6b49fd90db8f3ffbbd152"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export default app
+export default app;
